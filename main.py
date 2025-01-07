516a0810c41857653fe42b2f4d9b4ae3b584e29b
paxington181 <tsandrus@rogers.com>
2025-01-06 19:55:45 -0500

    Letter count lesson complete with less spaghetti code

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-def main(): # Still need to take the lower case letters and break them up into a dictionary - Jan 5th
+def main(): # Filter /n line returns
     book_location = "books/frankenstein.txt"
     words = book_text(book_location)
     word_count = word_counter(words)
     print(f"The book is {word_count} words")
     lower_case = small_letters(words)
-    l_words = split_lower(lower_case)
+    l_letters = letter_list(lower_case)
+    dictionary_populate = dictionary(l_letters)
+    print(dictionary_populate)
 
+def dictionary(l_letters):
+    letter_dictionary = {}
+    for letter in l_letters:
+        if letter in letter_dictionary:
+            letter_dictionary[letter] += 1
+        else:
+            letter_dictionary[letter] = 1
+    return letter_dictionary
+
+def letter_list(lower_case): # Breaks lower case letters into a list of all letters
+    letter = list(lower_case)
+    return letter
 
 def small_letters(words): # Convert the entire string to lower case
     l_case = words.lower()
     return l_case
-
-def split_lower(lower_case): # Breaks up the lower case string into single words
-    l_c_word = lower_case.split()
-    return l_c_word
 
 def word_counter(words): # Breaks the unedited string into single words then returns the word count
     word = words.split()
