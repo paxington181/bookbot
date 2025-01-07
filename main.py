ef291e7137a756f88e4b12086dbbc0b53c695f5f
paxington181 <tsandrus@rogers.com>
2025-01-07 12:43:32 -0500

    Finalized Book Project

--- main.py
+++ main.py
@@ -2,17 +2,30 @@ def main(): #Add dictionary to sorted list
     book_location = "books/frankenstein.txt"
     words = book_text(book_location)
     word_count = word_counter(words)
-    print(f"The book is {word_count} words")
     lower_case = small_letters(words)
     l_letters = letter_list(lower_case)
     dictionary_populate = dictionary(l_letters)
     sorted_dictionary = unsorted_to_sorted(dictionary_populate)
 
+    print(f"The book is {word_count} long.")
+    print()
+    print("The following is the count of each letter in the book")
+    for letter in sorted_dictionary:
+        if not letter["let"].isalpha():
+            continue
+        print(f"There are {letter['num']} of the letter '{letter['let']}'")
+    print()
+    print("This concludes the count.")
+
 def sort_on(dictionary_populate): # Required for the sort function to access dictionary entries
     return dictionary_populate["num"]
 
 def unsorted_to_sorted(dictionary_populate):
-
+    let_list = []
+    for l in dictionary_populate:
+        let_list.append({"let" : l, "num" : dictionary_populate[l]})
+    let_list.sort(reverse = True, key = sort_on)
+    return let_list
 
 def dictionary(l_letters): # Populates a dictionary of all letters and special characters
     letter_dictionary = {}
